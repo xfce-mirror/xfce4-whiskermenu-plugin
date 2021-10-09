b6fea3879a98569e59d7bd2228c66eb7a8ea0d4d
Matias De lellis <mati86dl@gmail.com>
2021-10-09 20:04:43 -0300

    Optionally use the account service to take the image of the user.
    On the other hand, round the image since all user managers use it like
    this.

--- panel-plugin/profile-picture.cpp
+++ panel-plugin/profile-picture.cpp
@@ -47,6 +47,19 @@ ProfilePicture::ProfilePicture(Window* window) :
 	Command* command = wm_settings->command[Settings::CommandProfile];
 	gtk_widget_set_tooltip_text(m_container, command->get_tooltip());
 
+#if HAVE_ACCOUNTSERVICE
+	m_act_um = act_user_manager_get_default();
+	gboolean loaded = FALSE;
+	g_object_get (m_act_um, "is-loaded", &loaded, nullptr);
+	if (loaded)
+	{
+		on_user_info_loaded (m_act_um, nullptr);
+	}
+	else
+	{
+		g_signal_connect_slot(m_act_um, "notify::is-loaded", &ProfilePicture::on_user_info_loaded, this);
+	}
+#else
 	gchar* path = g_build_filename(g_get_home_dir(), ".face", nullptr);
 	GFile* file = g_file_new_for_path(path);
 	g_free(path);
@@ -56,14 +69,20 @@ ProfilePicture::ProfilePicture(Window* window) :
 	on_file_changed(m_file_monitor, file, nullptr, G_FILE_MONITOR_EVENT_CHANGED);
 
 	g_object_unref(file);
+#endif
 }
 
 //-----------------------------------------------------------------------------
 
 ProfilePicture::~ProfilePicture()
 {
+#if HAVE_ACCOUNTSERVICE
+	g_object_unref (m_act_um);
+	g_object_unref (m_act_user);
+#else
 	g_file_monitor_cancel(m_file_monitor);
 	g_object_unref(m_file_monitor);
+#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -76,19 +95,103 @@ void ProfilePicture::reset_tooltip()
 
 //-----------------------------------------------------------------------------
 
-void ProfilePicture::on_file_changed(GFileMonitor*, GFile* file, GFile*, GFileMonitorEvent)
+static GdkPixbuf *round_pixbuf_file_at_size (const gchar* file, gint size)
 {
-	if (g_file_query_exists(file, nullptr))
+	GdkPixbuf *pixbuf = nullptr;
+	GdkPixbuf *dest = nullptr;
+	cairo_surface_t *surface;
+	cairo_t *cr;
+
+	pixbuf = gdk_pixbuf_new_from_file_at_size(file, size, size, nullptr);
+	if (!pixbuf)
+		return nullptr;
+
+	surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, size, size);
+	cr = cairo_create (surface);
+
+	cairo_arc (cr, size/2, size/2, size/2, 0, 2 * G_PI);
+	cairo_clip (cr);
+	cairo_new_path (cr);
+
+	gdk_cairo_set_source_pixbuf (cr, pixbuf, 0, 0);
+	cairo_paint (cr);
+
+	dest = gdk_pixbuf_get_from_surface (surface, 0, 0, size, size);
+	cairo_surface_destroy (surface);
+	cairo_destroy (cr);
+
+	g_object_unref(pixbuf);
+
+	return dest;
+}
+
+//-----------------------------------------------------------------------------
+
+static void set_file_picture(GtkWidget *image, const gchar* file)
+{
+	GdkPixbuf* pixbuf = nullptr;
+	if (file && g_file_test (file, G_FILE_TEST_EXISTS))
 	{
-		GIcon* icon = g_file_icon_new(file);
-		gtk_image_set_from_gicon(GTK_IMAGE(m_image), icon, GTK_ICON_SIZE_DND);
-		g_object_unref(icon);
+		pixbuf = round_pixbuf_file_at_size (file, 32);
+	}
+
+	if (pixbuf)
+	{
+		gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
+		g_object_unref(pixbuf);
 	}
 	else
 	{
-		gtk_image_set_from_icon_name(GTK_IMAGE(m_image), "avatar-default", GTK_ICON_SIZE_DND);
+		gtk_image_set_from_icon_name(GTK_IMAGE(image), "avatar-default", GTK_ICON_SIZE_DND);
 	}
 }
+
+#if HAVE_ACCOUNTSERVICE
+//-----------------------------------------------------------------------------
+
+void ProfilePicture::on_user_changed(ActUserManager*, ActUser* user)
+{
+	if (act_user_get_uid (user) != getuid ())
+		return;
+
+	set_file_picture (m_image, act_user_get_icon_file(user));
+}
+
+//-----------------------------------------------------------------------------
+
+void ProfilePicture::on_user_loaded(ActUser* user, GParamSpec*)
+{
+	on_user_changed(nullptr, user);
+}
+
+//-----------------------------------------------------------------------------
+
+void ProfilePicture::on_user_info_loaded(ActUserManager*, GParamSpec*)
+{
+	if (act_user_manager_no_service(m_act_um))
+	{
+		gtk_image_set_from_icon_name(GTK_IMAGE(m_image), "avatar-default", GTK_ICON_SIZE_DND);
+		return;
+	}
+
+	g_signal_connect_slot(m_act_um, "user-changed", &ProfilePicture::on_user_changed, this);
+
+	m_act_user = act_user_manager_get_user_by_id(m_act_um, getuid());
+	if (act_user_is_loaded (m_act_user)) {
+		on_user_changed(nullptr, m_act_user);
+	}
+	else {
+		g_signal_connect_slot(m_act_user, "notify::is-loaded", &ProfilePicture::on_user_loaded, this);
+	}
+}
+#else
+void ProfilePicture::on_file_changed(GFileMonitor*, GFile* file, GFile*, GFileMonitorEvent)
+{
+	gchar *path = g_file_get_path(file);
+	set_file_picture (m_image, path);
+	g_free (path);
+}
+#endif
 
 //-----------------------------------------------------------------------------
 
